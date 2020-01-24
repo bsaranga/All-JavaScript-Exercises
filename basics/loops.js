24545a9438f613c4998ea7829055f5fdbe0092c0
bsaranga <bsaranga@gmail.com>
2020-01-24 11:26:02 +0530

    loops

---
+++ basics/loops.js
@@ -0,0 +1,49 @@
+let i = 12
+while(i > 0){
+    console.log(`i = ${i}`);
+    i--;
+}
+
+do{
+    console.log('Executed atleast once');
+}while(i > 0);
+
+// For loops can have labels to identify the inner loops from the outer loops
+
+outer: for(let i = 0; ;i++){
+    inner: for(; ; i++){
+        if(i > 10) break inner;
+        console.log(`Currently i = ${i} from the inner loop`)
+    }
+    if(i > 20) break outer;
+    console.log(`Currently i = ${i} from the outer loop`)
+}
+
+let counter = 0;
+
+while(counter++ < 5){
+    console.log(`Counter incremented to ${counter} with post-fix incrementer`);
+}
+
+counter = 0;
+
+while(++counter < 5){
+    console.log(`Counter incremented to ${counter} with pre-fix incrementer`);
+}
+
+let result = '';
+for(let k = 0; k<9; k++){
+    result += String(k)
+} console.log(result)
+
+result = ''
+for(let k = 0; k<9; ++k){
+    result += String(k)
+} console.log(result)
+
+console.log('Outputting even numbers from 2 to 10');
+
+for(let lo = 0; lo<10;){
+    if(lo % 2 != 0){continue}else{console.log(lo)}
+    lo++;;
+}
