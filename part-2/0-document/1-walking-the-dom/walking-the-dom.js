c78a1451ff5961b0cefcec1336f65dd10a0b25af
bsaranga <bsaranga@gmail.com>
2020-04-23 21:48:20 +0530

    walking the dom

---
+++ part-2/0-document/1-walking-the-dom/walking-the-dom.js
@@ -0,0 +1,6 @@
+//document.body.style.background = 'red'; // sets the background red on page load.
+//setTimeout(() => document.body.style.background = '', 3000); // sets the background back to white after 3 sec
+
+for(let i = 0; i < document.body.childNodes.length; i++) {
+    console.log(document.body.childNodes[i]);
+}
