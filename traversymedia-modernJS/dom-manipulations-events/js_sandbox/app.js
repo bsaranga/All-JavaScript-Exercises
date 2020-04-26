a1f44b5a7f83f1e2f67d00a0e21b1437ca241cc9
bsaranga <bsaranga@gmail.com>
2020-04-26 13:43:15 +0530

    Traversing the dom

--- traversymedia-modernJS/dom-manipulations-events/js_sandbox/app.js
+++ traversymedia-modernJS/dom-manipulations-events/js_sandbox/app.js
@@ -1,13 +1,47 @@
 let val;
 
-val = document;
-val = document.all;
-val = document.all[7];
-val = document.all.length;
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
 
-let scripts = document.scripts;
-let scriptsArr = Array.from(scripts);
+val = listItem;
+val = list;
 
-scriptsArr.forEach((script) => console.log(script.getAttribute('src')));
+// Get child nodes
+
+val = list.childNodes; // returns a node list
+
+// Get children
+
+val = list.children; // returns an html collection
+
+val = list.childNodes[0].nodeName;
+val = list.childNodes[0].nodeType;
+
+val = list.children[3].children[0];
+
+val = list.firstChild; // includes non-elements.
+val = list.firstElementChild; // returns only elements.
+
+val = list.childElementCount; // returns the count of child elements
+
+console.clear();
+
+val = listItem.parentNode;
+val = listItem.parentElement;
+
+// Getting parents of parents
+
+val = listItem.parentElement.parentElement;
+
+// Getting siblings
+
+console.clear();
+
+val = listItem.nextSibling; // includes non-elements
+val = listItem.nextElementSibling;
+
+// Get Previous Sibling
+
+val = listItem.previousElementSibling; // null, because there's no previous element-sibling
 
 console.log(val);
