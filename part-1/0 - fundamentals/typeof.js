2701c28b3a487b6c7b8a15b5caceacde8fc04f83
bsaranga <bsaranga@gmail.com>
2020-10-24 20:21:41 +0530

    dir-res

--- part-1/0 - fundamentals/typeof.js
+++
@@ -1,8 +0,0 @@
-console.log(typeof(undefined)); // undefined
-console.log(typeof(null)); // object
-console.log(typeof(true)); // boolean
-console.log(typeof(1)); // number
-console.log(typeof(NaN)); // number
-console.log(typeof('string')); // string
-console.log(typeof(() => {})); // function
-console.log(typeof([])); // object
