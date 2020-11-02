3bae441025cadf84561f55a46aa34d229d06c085
bsaranga <bsaranga@gmail.com>
2020-11-02 09:25:13 +0530

    CreateBootcamp controller adds in data to the database

--- node-js/nodejs-api-masterclass/dev-camp/server.js
+++ node-js/nodejs-api-masterclass/dev-camp/server.js
@@ -17,6 +17,9 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
