66b1a4d41151ab83324d0e18bdfc6c4b60994f27
bsaranga <bsaranga@gmail.com>
2020-11-02 07:39:56 +0530

    Added and setup mongoDB Atlas, mongoose, and compass

--- node-js/nodejs-api-masterclass/dev-camp/server.js
+++ node-js/nodejs-api-masterclass/dev-camp/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
-// Load environment variables
-dotenv.config({path: './config/config.env'});
 const PORT = process.env.PORT;
 
 const app = express();
@@ -19,4 +24,10 @@ if(process.env.NODE_ENV === 'development'){
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandles promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+})
