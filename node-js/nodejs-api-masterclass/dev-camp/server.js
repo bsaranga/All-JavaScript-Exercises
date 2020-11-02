dddc01642d23e13aa64226fd873ebe2b9c4f95b4
bsaranga <bsaranga@gmail.com>
2020-11-02 06:53:14 +0530

    added routes, controllers, and middleware

--- node-js/nodejs-api-masterclass/dev-camp/server.js
+++ node-js/nodejs-api-masterclass/dev-camp/server.js
@@ -1,31 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load environment variables
-
 dotenv.config({path: './config/config.env'});
-
-const app = express();
 const PORT = process.env.PORT;
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success: true, msg: "Show all bootcamps"});
-});
+const app = express();
 
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Show bootcamp ${req.params.id}`});
-});
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success: true, msg: "Create new bootcamp"});
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`});
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`});
-});
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
