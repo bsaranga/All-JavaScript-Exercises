097e5ebf4c464ef6d8018835a24460503cb683d4
bsaranga <bsaranga@gmail.com>
2020-11-02 08:17:01 +0530

    downloaded exercise files

--- node-js/nodejs-api-masterclass/dev-camp/server.js
+++ node-js/nodejs-api-masterclass/dev-camp/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Load environment variables
@@ -24,10 +25,10 @@ if(process.env.NODE_ENV === 'development'){
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandles promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red.bold);
     server.close(() => process.exit(1));
 })
