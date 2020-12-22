137ce51188a9db87e686309800799bf3a83a6a4d
GitSaranga <bsaranga@gmail.com>
2020-12-22 13:15:00 +0530

    dir clean

--- javascript.info/part-1/6 - promises-async-await/promises/index.js
+++
@@ -1,5 +0,0 @@
-let promise = new Promise(function(resolve, reject){
-    setTimeout(() => resolve("It's done"), 1500);
-});
-
-promise.then(value => console.log(value), error => console.log(error));
