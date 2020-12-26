9eba6969c464471b485ed4b61e23a1a45dbffda1
bsaranga <bsaranga@gmail.com>
2020-12-26 11:31:46 +0530

    scope and execution

--- ydkjs/scope-and-closures/index.js
+++ ydkjs/scope-and-closures/index.js
@@ -1,18 +1,20 @@
-function foo() {
+var foo = "bar"
 
-    let a = "a on foo";
-    (bar())();
+function bar() {
+    var foo = "baz"
 
-    function bar() {
-
-        function coo() {
-            console.log(`_a_ here is: "${a}"`);
-        }
-
-        //coo();
-
-        return coo;
+    function baz(foo) {
+        foo = "bam"
+        bam = "yay"
     }
+
+    baz()
 }
 
-foo();
+bar(); // when bar is executed it executes baz within its scope, 'baz' has a parameter 'foo' which is considered just as a variable
+// on line 7, the execution engine asks "hey is there a variable foo?" the answer is yes, it was created as a parameter, and "bam"
+// is assigned to it. On line 8, the execution engine asks, "hey is there a variable bam?", the answer is no, so we go to the scope
+// of 'bar', and ask, "hey is there a variable bam?", nope. so we go to the global scope and create a variable 'bam' and assign "yay"
+
+console.log(bam); // prints "yay"
+console.log(foo); // prints "bar" ...if line 1 was commented, then this will throw a Reference Error
