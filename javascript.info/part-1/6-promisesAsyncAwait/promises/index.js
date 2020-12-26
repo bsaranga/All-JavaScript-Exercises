baa831dfb7a6054fffacef51e71d62710cb7b99a
bsaranga <bsaranga@gmail.com>
2020-12-26 19:28:45 +0530

    dir rename

---
+++ javascript.info/part-1/6-promisesAsyncAwait/promises/index.js
@@ -0,0 +1,13 @@
+let promise = new Promise(
+    
+    function(resolve, reject){
+        setTimeout(
+            () => resolve("It's done"), 1500
+        );
+    }
+
+);
+
+promise.then(
+    value => console.log(value), error => console.log(error)
+);
