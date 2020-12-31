f49bb4e23a4f63675208b13e0bd0071924cd48db
bsaranga <bsaranga@gmail.com>
2020-12-31 13:41:49 +0530

    express routing in detail

--- beyond-the-basics/routes-in-detail/index.js
+++ beyond-the-basics/routes-in-detail/index.js
@@ -3,12 +3,63 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello Freak Bitches\n')
+    res.status(200).send('Hello Freak Bitches\n')
 })
 
-app.post('/:param', (req, res) => {
-    console.log(req.params)
+app.get('/users/:userId/books/:bookId', (req, res) => {
+    console.log(req.params);
+    res.send('');
 })
+
+app.get('/flights/:from-:to', (req, res) => {
+    console.log(req.params);
+    res.send('');
+})
+
+app.get('/plantae/:genus.:species', (req, res) => {
+    console.log(req.params);
+    res.send('');
+})
+
+app.get('/example/a', function (req, res, next){
+    console.log('Response will be send by next func')
+    next()
+}, function (req, res) {
+    res.send('Hello from NeXt\n')
+})
+
+function factorial(x) {
+    if(x === 0) {
+        return 1
+    }
+    return x*factorial(x-1);
+}
+
+app.get('/factorial/:num', (req, res, next) => {
+    res.send(`Factorial: ${factorial(Number(req.params.num))}\n`)
+})
+
+function first(req, res, next){
+    console.log('Executed first function')
+    next()
+}
+
+function second(req, res, next) {
+    console.log('Executed second function')
+    next()
+}
+
+function third(req, res) {
+    res.send('Executed the last function in the pipeline\n')
+}
+
+app.get('/pipeline', [first, second, third]);
+
+app.route('/flex')
+    .get((req, res) => res.send('Get flex objects'))
+    .post((req, res) => res.send('Create flex objects'))
+    .put((req, res) => res.send('Update flex objects'))
+
 
 app.listen(port,() => {
     console.log(`Listening at http://localhost:${port}`)
