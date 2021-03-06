a85da011d74f6b4c63ea4014bc16cb7a5580f2b6
bsaranga <bsaranga@gmail.com>
2021-03-06 10:06:29 +0530

    handlebars view engine setup

---
+++ web-dev-express-node/p1/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const expressHandleBars = require('express-handlebars')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.engine('handlebars', expressHandleBars({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine', 'handlebars')
+
+app.use(express.static(`${__dirname}/public`))
+
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => res.render('about'))
+
+// custom 404 page
+app.use((req, res) => {
+    res.status(404)
+    res.render('404')
+})
+
+// custom 500 page
+app.use((err, req, res, next) => {
+    console.log(err.message)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(port, () => console.log(`Express started on http://localhost:${port}`))
