3e1221160e9e0282e5a343b336d2ea1e9a8113b6
GitSaranga <bsaranga@gmail.com>
2021-03-19 19:07:37 +0530

    js the good parts only

--- javascript.info/part-1/6-promisesAsyncAwait/callbacks/loader.js
+++
@@ -1,8 +0,0 @@
-function loadScript(src, callback) {
-    let script = document.createElement('script');
-    script.src = src;
-    script.onload = () => callback(script);
-    document.head.append(script);
-    console.log("Script Loaded");
-    console.log(script);
-}
